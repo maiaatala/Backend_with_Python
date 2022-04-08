7aadbea926040d1fdfae3d1c1d4b19691c316965
maiaatala <ana.atala@unemat.br>
2022-04-08 18:23:10 -0400

    update route access

--- 06_Library_FastAPI/app.py
+++ 06_Library_FastAPI/app.py
@@ -1,5 +1,6 @@
 # package dependencies
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # my_package imports
 import routes as r
@@ -14,6 +15,16 @@ Organize all your books by titles, authors, publishers or genre!
 Ana C. M. Atala
 """
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000/",
+    " http://127.0.0.1:8000",
+    "*",
+]
+
 app = FastAPI(title="Library", description=desc)
 
 init_db()
@@ -22,6 +33,14 @@ app.include_router(r.auth_api)  # metodo para incluir as rotas auxiliares
 app.include_router(r.book_api)
 app.include_router(r.category_api)
 app.include_router(r.cat_book_api)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # lazy loading sql alchemy to show author's information when getting books
